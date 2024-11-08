93079ee532e0f159ac91d91f53c05359e457ebc1
Bryan Muoki <bryanmuoki29@gmail.com>
2024-11-08 11:05:26 +0300

    Update Instructions.jsx

---
+++ src/Components/Instructions.jsx
@@ -0,0 +1,15 @@
+// components/Instructions.js
+import React from 'react';
+
+const Instructions = () => {
+  return (
+    <section>
+      <h2>How to Play</h2>
+      <a href="https://www.youtube.com/watch?v=-A-djjimCcM&t=41s&ab_channel=MissBrain%27sCoolMathVideos">
+        How To Play
+      </a>
+    </section>
+  );
+};
+
+export default Instructions;
