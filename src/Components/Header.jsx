2ee59331427c0cb3329b5ff7ef8fddf18001cfdb
--add <168851515+nelly2040@users.noreply.github.com>
2024-11-08 11:04:07 +0300

    home & header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <nav>
+        <a href="/">Home</a>
+        <a href="#about">About</a>
+        <a href="#game-board">Play Mancala</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
