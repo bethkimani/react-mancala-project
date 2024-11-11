01fd507d7e1e36e635215e542d9b55828b7996b3
Beth Kimani <kbwaceke@gmail.com>
2024-11-11 11:17:27 +0300

    Save local changes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 // App.js
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+
+import Header from "./components/Header";
+
+
+
 import Home from './components/Home';
 import About from './components/About';
 import GameBoard from './components/GameBoard';
